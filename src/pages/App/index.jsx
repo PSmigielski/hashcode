0eeb9e62448e0efad91ca51819cdfba0bb9a9117
psmigielski <walek.smigielski@gmail.com>
2022-01-02 16:08:17 +0100

    add menu and styling to home

--- src/pages/App/index.jsx
+++ src/pages/App/index.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../../components/Footer";
+import Menu from "../../components/Menu";
 import Navbar from "../../components/Navbar";
 import "./index.css";
 
@@ -7,6 +8,7 @@ const App = () => {
     return (
         <div className="app">
             <Navbar />
+            <Menu />
             <Outlet />
             <Footer />
         </div>
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,25 @@
+
 import "./index.css";
 const Home = () => {
     return (
-        <div className="Home">
-            
+        <div className="home">
+            <div className="home__content">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit odit perspiciatis eligendi nisi nobis facere aliquam voluptatem in dolor est alias voluptatum, nostrum doloribus laboriosam. Eaque perferendis dicta esse impedit?
+                Eligendi assumenda beatae magnam porro fugit impedit at. Provident sit beatae quos dolor praesentium, veritatis sunt eius architecto ullam fugiat reiciendis? Incidunt provident libero, cumque nam quidem nemo reprehenderit ex.
+                Adipisci, dolorem consectetur! Culpa placeat tenetur facilis quisquam rerum ut accusamus iste, assumenda harum quibusdam aliquam mollitia impedit corporis cumque hic ab tempora? Suscipit autem eaque iure neque odio! Id?
+                Facilis quos, deserunt assumenda modi iste quasi, dolorem error nemo quisquam ratione accusantium commodi illo id repellat mollitia eaque animi. Amet neque repellat consequatur ut harum reiciendis fuga doloribus accusantium!
+                Iure doloremque debitis reprehenderit non veritatis delectus tempora. Distinctio aspernatur laborum consequuntur dolores error repudiandae repellat dicta ullam pariatur nemo sequi amet magnam voluptatibus ex, temporibus, placeat id vitae cupiditate.
+                Nihil distinctio fugit atque sint voluptatem laudantium, quod vero cum esse, aperiam, cumque saepe dolorem molestiae rem nisi culpa labore inventore neque autem quae ipsum tempora nobis. Nisi, quod velit?
+                Rerum, nesciunt autem temporibus nam facere animi officia non, molestiae quo quisquam minus odio? Eveniet laudantium, dolorum nesciunt culpa odio architecto quod, rerum non fuga delectus eius voluptatum ratione esse.
+                Odio iste molestiae cumque repudiandae! Enim quaerat, delectus porro eum perferendis minus doloribus explicabo, consequuntur deleniti, temporibus blanditiis aspernatur cum ut magni impedit tempora unde tenetur autem. Impedit, neque natus?
+                Numquam, cupiditate fugit facere, mollitia itaque reprehenderit magnam sapiente, quo praesentium nam veniam tenetur voluptatibus inventore sequi? Dolorum libero sunt voluptates sapiente dicta voluptatem, nemo numquam, repellat obcaecati quibusdam veritatis.
+                Suscipit, neque laudantium! Quae beatae adipisci repellat doloribus. Vitae est itaque nostrum molestiae quod quos placeat, quae mollitia totam quam animi tempore deleniti iste quo, voluptas doloribus maxime similique exercitationem.
+                Dolore a quae ducimus laboriosam rerum at, eligendi iste laborum nemo! Quia architecto unde, odio explicabo magni atque placeat dicta officia! Distinctio dolorem labore voluptatum nostrum optio enim? Alias, voluptate!
+                Earum esse praesentium velit eaque, nostrum nulla at mollitia illo odio quae error eveniet tenetur enim assumenda eligendi ab hic laudantium quod corrupti ipsam iure quis amet iste. Nobis, sint.
+                Exercitationem voluptatum ipsa magni unde similique architecto et, provident dolorum consequuntur, deserunt optio vel fugit accusamus, harum laudantium consequatur assumenda. Deserunt quae veniam reiciendis similique doloremque magni numquam praesentium est.
+                Delectus doloremque unde, nobis quisquam omnis, ea est dignissimos, magni quam fuga laborum. Perspiciatis provident sed repudiandae voluptas minima pariatur, quis odio iste beatae, dolores laudantium illo qui, autem distinctio?
+                Nam vitae reiciendis natus eos error placeat quam voluptatum quis illum possimus quasi sunt blanditiis facilis, expedita reprehenderit est excepturi cum quaerat ipsa, consectetur aliquam architecto dolor ipsam veritatis. Impedit?
+            </div>
         </div>
     )
 }
