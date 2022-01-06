d09173be0572f04bad140d4e21b1030aedc1b22e
psmigielski <walek.smigielski@gmail.com>
2022-01-06 16:31:09 +0100

    add 404 page

---
+++ src/pages/NotFound/index.jsx
@@ -0,0 +1,67 @@
+import { Link } from "react-router-dom";
+import "./index.css";
+
+const NotFound = () => {
+    return (
+        <div className="NotFound">
+            <h1 className="NotFound__header">404</h1>
+            <p className="NotFound__errorName">Not Found</p>
+            <Link className="NotFound__Link" to="/"><button className="NotFound__button">Wróć na stronę główną</button></Link>
+        </div>
+    )
+}
+
+export default NotFound;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
